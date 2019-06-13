2906b34d56000cccc6928df5cc74de531bcb0f71
Gabriele Greco <gabriele.greco@wyscout.com>
2019-06-13 10:57:22 +0200

    Initial Commit

---
+++ TestHybrid/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TestHybrid
+//
+//  Created by Gabriele Greco on 13/06/2019.
+//  Copyright © 2019 Gabriele Greco. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
